26deb976cef3d5478e8e3080e97d2bf14e19ff82
Martin <mhaan@student.codam.nl>
2024-11-11 13:16:49 +0100

    added nodemodules to gitignore

--- pong_backend/src/app.module.ts
+++ pong_backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SinglePlayerGateway } from './pong/single-player/single-player.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SinglePlayerGateway],
 })
 export class AppModule {}
